f50ee9738e1aa0d671338ac70fcde99e61210bd1
Sinan Sarı <sinn.sari@gmail.com>
2019-01-19 14:30:22 +0300

    Dictionary Api

---
+++ DictionaryV2.WebApi/Controllers/DictionaryController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DictionaryV2.Business.Abstract;
+using DictionaryV2.Entity.Concreate;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DictionaryV2.WebApi.Controllers
+{
+    public class DictionaryController : Controller
+    {
+        private IEngDictionaryService _engDictionaryService;
+
+        public DictionaryController(IEngDictionaryService engDictionaryService) {
+            _engDictionaryService = engDictionaryService;
+        }
+
+        [HttpGet]
+        public List<EngDictionary> Get() {
+
+            return _engDictionaryService.GetAll();
+        }
+    }
+}
---
+++ DictionaryV2.WebApi/Startup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DictionaryV2.Business.Abstract;
+using DictionaryV2.Business.Concreate;
+using DictionaryV2.DataAccess.Abstract;
+using DictionaryV2.DataAccess.Concreate.EntityFramework;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace DictionaryV2.WebApi {
+    public class Startup {
+        public Startup(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddMvc();
+
+            services.AddDbContext<DictionaryContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddTransient<IEngDictionaryService, EngDictionaryManager>();
+            services.AddTransient<IEngDictionaryDal, EfEngDictionaryDal>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+            if (env.IsDevelopment()) {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc(routes => {
+                routes.MapRoute(name: "default", template: "api/{controller=dictionary}/{action=get}");
+            });
+        }
+    }
+}
